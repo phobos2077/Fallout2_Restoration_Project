810bffec5e8e32002f0296492c15e6cd6a6db3ef
Magus <magus@bgforge.net>
2019-07-10 01:02:53 +0700

    ...

--- extra/publish.sh
+++ extra/publish.sh
@@ -9,13 +9,13 @@ if [ -n "$TRAVIS_TAG" ]; then # tag found: releasing
   # data
   dst="$mods_dir/$mod_name.dat"
   mkdir -p "$dst"
-  mv data/* "$dst/"
+  chmod 0444 data/proto/*/*
+  mv -f data/* "$dst/"
 
   # sfall
   sfall_url="https://sourceforge.net/projects/sfall/files/sfall/sfall_$sfall_version.7z/download"
   wget -q "$sfall_url" -O sfall.7z
   7z e sfall.7z ddraw.dll
   cp extra/sfall/ddraw.ini .
-  chmod 0444 "$mods_dir"/data/proto/*/*
   7z a "${mod_name}_${version}.7z" ddraw.dll ddraw.ini "$mods_dir/" # our package
 fi
