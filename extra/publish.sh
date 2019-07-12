912748bdae45543265f5b3bd8706cc0821c18dab
Magus <magus@bgforge.net>
2019-07-12 16:15:43 +0700

    fixed dat directory level

--- extra/publish.sh
+++ extra/publish.sh
@@ -7,14 +7,17 @@ if [ -n "$TRAVIS_TAG" ]; then # tag found: releasing
   version="$TRAVIS_TAG"
 
   # data
-  dat="$mods_dir/$mod_name.dat"
+  dat="$mod_name.dat"
   mkdir -p "$mods_dir"
   chmod 0444 data/proto/*/*
 
+  cd data
   # I don't know how to pack recursively
-  for f in $(find data -type f | sed 's|\/|\\|g' | sort); do # replace slashes with backslashes
+  for f in $(find . -type f | sed -e 's|^\.\/||' -e 's|\/|\\|g' | sort); do # replace slashes with backslashes
     WINEARCH=win32 WINEDEBUG=-all wine "$bin_dir/dat2.exe" a $dat "$f"
   done
+  cd ..
+  mv "data/$dat" "$mods_dir/"
 
   # sfall
   sfall_url="https://sourceforge.net/projects/sfall/files/sfall/sfall_$sfall_version.7z/download"
