ce6291a68c5fef00fc5d44927690929b9dba5a1a
Magus <magus@bgforge.net>
2019-07-14 13:05:58 +0700

    Stringification from UP, enabled optimization

--- extra/buildall.sh
+++ extra/buildall.sh
@@ -16,7 +16,7 @@ function process_file() {
   script_name="$(echo "$f" | tr "[A-Z]" "[a-z]" | sed 's|\.ssl$|.int|')" # lowercase
   WINEARCH=win32 WINEDEBUG=-all wine "$bin_dir/wcc386.exe" "$f" -p -fo="$f.tmp" -w  # preprocess
   sed -i '/^[[:space:]]*$/d' "$f.tmp" # delete empty lines
-  WINEARCH=win32 WINEDEBUG=-all wine "$bin_dir/compile.exe" -n -l -q "$f.tmp" -o "$dst/$script_name" # compile
+  WINEARCH=win32 WINEDEBUG=-all wine "$bin_dir/compile.exe" -n -l -q -O2 "$f.tmp" -o "$dst/$script_name" # compile
   rm -f "$f.tmp"
 }
 # compile all
