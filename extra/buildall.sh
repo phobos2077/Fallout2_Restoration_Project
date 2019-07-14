dc195ea2a4390e2badbc9d2968d83e9f3cae6726
Magus <magus@bgforge.net>
2019-07-14 16:44:33 +0700

    parallel processing

--- extra/buildall.sh
+++ extra/buildall.sh
@@ -18,21 +18,30 @@ mkdir -p "$dst"
 # single file compile
 function process_file() {
   f="$1"
+  dst="$2"
   script_name="$(echo "$f" | tr "[A-Z]" "[a-z]" | sed 's|\.ssl$|.int|')" # lowercase
   WINEARCH=win32 WINEDEBUG=-all wine "$bin_dir/wcc386.exe" "$f" -p -fo="$f.tmp" -w  # preprocess
   sed -i '/^[[:space:]]*$/d' "$f.tmp" # delete empty lines
   WINEARCH=win32 WINEDEBUG=-all wine "$bin_dir/compile.exe" -n -l -q -O2 "$f.tmp" -o "$dst/$script_name" # compile
   rm -f "$f.tmp"
 }
+export -f process_file
+
 # compile all
 for d in $(ls $src); do
   if [[ -d "$src/$d" && "$d" != "TEMPLATE" ]]; then # if it's a dir and not template
     cd "$src/$d"
-    for f in $(ls | grep -i "\.ssl$"); do
-      if ! grep -qi "$d/$f" "$skip_list"; then # check if file is not on skip list
-        process_file "$f"
+    files=""
+    set +x # ok this is too verbose
+    for f in $(ls | grep -i "\.ssl$"); do # build file list
+      if ! grep -qi "$d/$f" "$skip_list"; then # check if file is not on skip list. Ugly but works.
+        files="$files $f"
       fi
     done
+    set -x # enabling output again
+    if [[ -n "$files" ]]; then
+      parallel.moreutils -j2 -i bash -c "process_file {} $dst" -- $files
+    fi
     cd ..
   fi
 done
