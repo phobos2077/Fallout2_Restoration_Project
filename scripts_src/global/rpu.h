bac759c1120a3cb8ee30cbd458727568ff87982b
Magus <magus@bgforge.net>
2019-07-17 22:26:54 +0700

    added scripting support for Marcus' and Dogmeat's armor

--- scripts_src/global/rpu.h
+++ scripts_src/global/rpu.h
@@ -1,6 +1,10 @@
 #define NAME "rpu"
 #define rpu_ini "mods\\rpu.ini"
-#define rpu_setting(section, setting) get_ini_setting(rpu_ini + "|" + section + "|" + setting)
-#define rpu_string(section, setting) get_ini_string(rpu_ini + "|" + section + "|" + setting)
 #define ndebug(message) debug_msg(NAME + ": " + message + "\n")
 #define sec_main "main"
+#define rpu_setting(section, setting) get_ini_setting(rpu_ini + "|" + section + "|" + setting)
+#define rpu_string(section, setting) get_ini_string(rpu_ini + "|" + section + "|" + setting)
+#define rpu_msetting(setting) get_ini_setting(rpu_ini + "|" + sec_main + "|" + setting)
+
+#define set_marcus_armor "marcus_armor"
+#define set_dogmeat_armor "dogmeat_armor"
