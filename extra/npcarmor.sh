4dc353695a6dd8493b4cb1fdb6faa6065cb95d8a
Magus <magus@bgforge.net>
2019-07-21 18:11:25 +0700

    typo

--- extra/npcarmor.sh
+++ extra/npcarmor.sh
@@ -5,7 +5,7 @@ set -xeu -o pipefail
 release_dir="$(realpath $release_dir)"
 scripts_dir="$release_dir/data/scripts"
 
-mkdir -p "$scritps_dir"
+mkdir -p "$scripts_dir"
 7z e "$mpack_7z" mods/gl_npcarmor.int
 mv gl_npcarmor.int "$scripts_dir"
 7z e "$mpack_7z" mods/npcarmor.ini
