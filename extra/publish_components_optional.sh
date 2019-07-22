325d145ea48cd0e35b00c4b67ebc9b5e398f20da
Magus <magus@bgforge.net>
2019-07-23 00:14:53 +0700

    add version to component download filename

--- extra/publish_components_optional.sh
+++ extra/publish_components_optional.sh
@@ -9,11 +9,13 @@ file_list="$(realpath file.list)"
 mods_dir="$(realpath $mods_dir)"
 release_dir="$(realpath release_dir)"
 
+version="$TRAVIS_TAG"
+
 if [[ -d "$comp_dir" ]]; then
   cd "$comp_dir"
   for c in $(ls); do
     dat="${mod_name}_$c.dat"
-    zip="${mod_name}_$c.zip"
+    zip="${mod_name}_${version}_$c.zip"
     cd "$c"
     find . -type f | sed -e 's|^\.\/||' -e 's|\/|\\|g' | sort > "$file_list"
     $dat2 a "$dat" @"$file_list"
