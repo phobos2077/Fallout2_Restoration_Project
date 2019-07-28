01d9e91ef8a5cce4b1dc1026e771b9f8478d0021
Magus <magus@bgforge.net>
2019-07-28 17:15:30 +0700

    added a batch file for installation, too

---
+++ release/rpu-install.bat
@@ -0,0 +1,25 @@
+set bdir="backup.rpu"
+
+mkdir %bdir%\data
+
+if exist patch000.dat (
+  move patch000.dat %bdir%\
+)
+
+:: I don't know how to iterate over files in batch properly, so just moving back and forth
+move data %bdir%\
+move %bdir%\data\savegame data\
+move %bdir%\data\*.txt data\
+move %bdir%\data\*.gcd data\
+move %bdir%\data\sound data\
+
+:: old appearance mod in directories
+set appearances=hfr00s00,hfr00s01,hfr00s02,hmr00s00,hmr00s01,hmr00s02
+for %%a in (%appearances%) do (
+  if exist appearance\%%a (
+    if not exist %bdir%\appearance mkdir %bdir%\appearance
+    move appearance\%%a %bdir%\appearance\
+  )
+)
+
+echo "RPU installed."
