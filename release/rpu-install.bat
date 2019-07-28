ff4beffc7f3de06940bac848c1d5f863433755cf
Magus <magus@bgforge.net>
2019-07-28 17:42:42 +0700

    don't show errors if files/dirs don't exist

--- release/rpu-install.bat
+++ release/rpu-install.bat
@@ -8,10 +8,10 @@ if exist patch000.dat (
 
 :: I don't know how to iterate over files in batch properly, so just moving back and forth
 move data %bdir%\
-move %bdir%\data\savegame data\
-move %bdir%\data\*.txt data\
-move %bdir%\data\*.gcd data\
-move %bdir%\data\sound data\
+move %bdir%\data\savegame data\ 2>nul
+move %bdir%\data\*.txt data\ 2>nul
+move %bdir%\data\*.gcd data\ 2>nul
+move %bdir%\data\sound data\ 2>nul
 
 :: old appearance mod in directories
 set appearances=hfr00s00,hfr00s01,hfr00s02,hmr00s00,hmr00s01,hmr00s02
