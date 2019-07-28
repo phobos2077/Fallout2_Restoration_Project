b1c2a0361375d42f3da42aa396946ff109d8420e
Magus <magus@bgforge.net>
2019-07-28 20:49:47 +0700

    don't need to rebuild on translations

--- extra/need-build.sh
+++ extra/need-build.sh
@@ -10,7 +10,7 @@ if [[ -n "$TRAVIS_TAG" ]]; then
 fi
 
 TRAVIS_COMMIT_RANGE=${TRAVIS_COMMIT_RANGE:-}
-if git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qv "data\|docs\|README.md\|release\|appearance" ; then
+if git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qv "data\|docs\|README.md\|release\|appearance\|translations" ; then
   exit 0
 fi
 
