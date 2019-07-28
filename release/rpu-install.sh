15d9b38d7e83d4e3b7be02fcef378bed10405d3c
Magus <magus@bgforge.net>
2019-07-28 16:00:14 +0700

    disable old appearance mod if present

--- release/rpu-install.sh
+++ release/rpu-install.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-bdir="data.rpu.bak"
+bdir="backup.rpu"
 
-rm -f patch000.dat
-mv data $bdir
-mkdir -p data/sound
-mv $bdir/SAVEGAME data/
-mv $bdir/sound/music data/sound/
-for ext in txt TXT gcd GCD; do
-if [[ "$(ls $bdir | grep -c $ext)" -gt 0 ]]; then
-  mv $bdir/*.$ext data/
+if [[ -f patch000.dat ]]; then
+  mv patch000.dat $bdir/
 fi
+
+mkdir -p $bdir/data
+
+# keep savegames, sound, character files
+for i in $(ls data | grep -vi "^savegame$\|\.txt$\|\.gcd$\|^sound$"); do
+  mv data/$i $bdir/data/
+done
+
+# old appearance mod in directories
+if [[ -d appearance ]]; then
+  for i in $(ls appearance); do
+    if [[ -d $i ]]; then
+      mv appearance/$i $bdir/
+    fi
+  done
+fi
+
 echo "RPU installed."
