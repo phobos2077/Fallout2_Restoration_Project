8e055aebdaca7a04560a2542035856ab9b55896c
Magus <magus@bgforge.net>
2019-09-26 18:19:14 +0700

    download appearance package from its own repo, see #14.

--- extra/publish/appearance.sh
+++ extra/publish/appearance.sh
@@ -3,19 +3,9 @@
 set -xeu -o pipefail
 
 bin_dir="$(realpath $bin_dir)"
-dat2a="wine $bin_dir/dat2.exe a -1"
-file_list="$(realpath file.list)"
 release_dir="$(realpath $release_dir)"
 
-if [[ -d "$appearance_dir" ]]; then
-  mkdir -p "$release_dir/appearance"
-  cd "$appearance_dir"
-  for a in $(ls); do
-    dat="$a.dat"
-    cd "$a"
-    find . -type f | sed -e 's|^\.\/||' -e 's|\/|\\|g' | sort > "$file_list"
-    $dat2a "$release_dir/appearance/$dat" @"$file_list"
-    cd ..
-  done
-  cd ..
-fi
+cd "$release_dir"
+wget "$(curl -s https://api.github.com/repos/${appearance_repo}/releases/latest | grep browser_download_url | awk -F '"' '{print $4}')"
+unzip appearance*.zip
+rm -f appearance*.zip
