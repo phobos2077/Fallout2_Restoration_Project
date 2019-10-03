67018ee3d40cbfb1873956822310324660070b97
Magus <magus@bgforge.net>
2019-10-03 21:35:20 +0700

    disable need-build, same as UP

--- extra/buildall.sh
+++ extra/buildall.sh
@@ -8,11 +8,6 @@ extra_dir="$(realpath extra)"
 bin_dir="$(realpath $bin_dir)"
 skip_list="$(realpath $extra_dir/compile_skip.list)"
 
-if ! $extra_dir/need-build.sh ; then
-  echo "scripts haven't changed, skipping build"
-  exit 0
-fi
-
 mkdir -p "$dst"
 
 # single file compile
