137558cc53ef4e71e672e19691e0f22fb2799e3e
Magus <magus@bgforge.net>
2019-11-11 19:58:37 +0700

    always use latest ddraw.ini, apply RP changes on top

--- extra/publish/sfall.sh
+++ extra/publish/sfall.sh
@@ -3,6 +3,9 @@
 set -xeu -o pipefail
 
 release_dir="$(realpath $release_dir)"
+extra_dir="$(realpath $extra_dir)"
+release_ini="$release_dir/ddraw.ini"
+up_ini="$extra_dir/publish/ddraw.ini"
 
 sfall_url="https://sourceforge.net/projects/sfall/files/sfall/sfall_$sfall_version.7z/download"
 files="
@@ -19,3 +22,8 @@ for f in $files; do
   7zr e sfall.7z "$f"
   mv "$(basename $f)" "$release_dir/"
 done
+
+crudini --merge "$release_ini" < "$up_ini"
+version="$(echo $TRAVIS_TAG | tr 'v' 'u')"
+crudini --set "$release_ini" "Misc" "VersionString" "FALLOUT II 1.02d  RP 2.3.3${version}"
+unix2dos "$release_ini"
