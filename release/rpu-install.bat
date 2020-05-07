5b6eee79d2203e6a4ebd966f2f2b57c8d253e692
Magus <magus@bgforge.net>
2020-05-08 02:16:38 +0700

    updated release scripts from upu

--- release/rpu-install.bat
+++ release/rpu-install.bat
@@ -1,5 +1,3 @@
-@echo off
-
 set "bdir=backup\rpu"
 set "scripts_dir=data\scripts"
 
@@ -10,11 +8,11 @@ if exist patch000.dat (
 )
 
 :: I don't know how to iterate over files in batch properly, so just moving back and forth
-move data %bdir%\
-move %bdir%\data\savegame data\ 2>nul
-move %bdir%\data\*.txt data\ 2>nul
-move %bdir%\data\*.gcd data\ 2>nul
-move %bdir%\data\sound data\ 2>nul
+move /y data %bdir%\
+move /y %bdir%\data\savegame data\
+move /y %bdir%\data\*.txt data\
+move /y %bdir%\data\*.gcd data\
+move /y %bdir%\data\sound data\
 
 :: old appearance mod in directories
 set appearances=hfr00s00,hfr00s01,hfr00s02,hmr00s00,hmr00s01,hmr00s02
@@ -35,11 +33,10 @@ move gl_partycontrol.int %scripts_dir%\
 :: sound path fix
 initool.exe s fallout2.cfg sound music_path1 data\sound\music\ > tmp.cfg
 initool.exe s tmp.cfg sound music_path2 sound\music\ > tmp2.cfg
-move fallout2.cfg %bdir%\
-del /s tmp.cfg initool.exe camlrt.dll > nul 2>&1
-ren tmp2.cfg fallout2.cfg
+del /s %bdir%\fallout2.cfg
+move /y fallout2.cfg %bdir%\
+del /s tmp.cfg initool.exe camlrt.dll
+move /y tmp2.cfg fallout2.cfg
 
-echo RPU installed. Backup is in %bdir%
-pause
-del /s rpu-install.sh rpu-install.bat rpu-install.command > nul 2>&1
-(goto) 2>nul & del "%~f0"
+del /s rpu-install.sh rpu-install.bat rpu-install.command
+echo UPU installed. Backup is in %bdir%
