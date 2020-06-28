76f2c3b25a79d7a2bab9ceeea52cd86bb3bedc2a
Magus <magus@bgforge.net>
2020-06-29 01:45:39 +0700

    fix hq sound dir

--- extra/publish/hq_music.sh
+++ extra/publish/hq_music.sh
@@ -4,8 +4,10 @@ set -xeu -o pipefail
 
 bin_dir="$(realpath $bin_dir)"
 release_dir="$(realpath $release_dir)"
+sound_dir="$release_dir/data/sound"
 
-cd "$release_dir"
+mkdir -p "$sound_dir"
+cd "$sound_dir"
 wget -nv "$(curl -s https://api.github.com/repos/${hq_music_repo}/releases/latest | grep browser_download_url | grep 'fallout12_hq_music' | awk -F '"' '{print $4}')"
 unzip fallout12_hq_music*.zip
 rm -f fallout12_hq_music*.zip
