5e0816e00a09906cb879fd7c26a9597837a1e54d
Magus <magus@bgforge.net>
2020-07-04 23:35:31 +0700

    include all files from sfall, ref https://forums.bgforge.net/viewtopic.php?f=34&p=760

--- extra/publish/sfall.sh
+++ extra/publish/sfall.sh
@@ -6,23 +6,18 @@ release_ini="$release_dir/ddraw.ini"
 custom_ini="$extra_dir/publish/ddraw.ini"
 
 sfall_url="https://sourceforge.net/projects/sfall/files/sfall/sfall_$sfall_version.7z/download"
-files="
-ddraw.dll
-ddraw.ini
-sfall.dat
-"
 
+cd "$release_dir"
 wget -nv "$sfall_url" -O sfall.7z
-for f in $files; do
-  7zr e sfall.7z "$f"
-  mv "$(basename $f)" "$release_dir/"
-done
+7z x sfall.7z
+rm -f sfall.7z
 
 # uncomment ini settings to preserve options' placement in ddraw.ini
 entries="$(cat $custom_ini | grep '=' | awk -F '=' '{print $1}')"
 for e in $entries; do
   sed -i "s|^;$e=|$e=|" "$release_ini"
 done
+
 # then merge custom settings
 crudini --merge "$release_ini" < "$custom_ini"
 # set version string
