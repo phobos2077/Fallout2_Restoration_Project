51f1d6bf7aeefc3fbf953420145491341b057803
Magus <magus@bgforge.net>
2021-08-31 17:50:42 +0700

    debug #119

--- extra/buildall.sh
+++ extra/buildall.sh
@@ -15,6 +15,7 @@ if [[ ! -f $compile_exe ]]; then
 fi
 
 # compile all
+gcc -v
 for d in $(ls $src); do
   if [[ -d "$src/$d" && "$d" != "template" ]]; then # if it's a dir and not a template
     cd "$src/$d"
@@ -23,6 +24,10 @@ for d in $(ls $src); do
       int="$(echo $f | sed 's|\.ssl$|.int|')"
       if grep -qi "^$int " "$scripts_lst" || [[ "$d" == "global" ]]; then # if file is in scripts.lst or a global script
         script_name="$(echo "$f" | sed 's|\.ssl$|.int|')"
+
+        ec=$(gcc -E -x c -P -Werror -Wfatal-errors -o "${f}.tmp" "$f")
+        echo "gcc exit code = $ec"
+
         gcc -E -x c -P -Werror -Wfatal-errors -o "${f}.tmp" "$f" # preprocess
         wine "$compile_exe" -n -l -q -O2 "$f.tmp" -o "$dst/$script_name" # compile
       fi
