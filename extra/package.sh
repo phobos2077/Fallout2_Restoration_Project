10007806d6db3f8211bd8796497cfc95d0310c8c
Magus <magus@bgforge.net>
2021-09-01 16:28:34 +0700

    keep bat in manual installer, in case inno doesn't work on some older machine

--- extra/package.sh
+++ extra/package.sh
@@ -78,8 +78,6 @@ cd "$release_dir"
 zip -r "${mod_name}_${vversion}.zip" * # our package
 popd
 mv "$release_dir/${mod_name}_${vversion}.zip" .
-# bat is only used by innosetup
-zip -d "${mod_name}_${vversion}.zip" "rpu-install.bat"
 
 # exe installer
 "$extra_dir"/package/inno.sh
