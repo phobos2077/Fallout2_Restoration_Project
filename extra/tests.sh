b56837c8c3e0e157e5f009c3e0a6ce9d78da597f
Magus <magus@bgforge.net>
2022-05-19 19:23:27 +0700

    dialog fixed from up

--- extra/tests.sh
+++ extra/tests.sh
@@ -4,3 +4,4 @@ set -xeu -o pipefail
 
 ./extra/tests/scripts-lst.py scripts_src/headers/scripts.h data/scripts/scripts.lst
 ./extra/tests/lvars.py data/scripts/scripts.lst scripts_src
+./extra/tests/dialogs.py
