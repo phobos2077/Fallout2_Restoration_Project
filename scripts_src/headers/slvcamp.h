5c6f5fc0ea6320a89377021ce420c59b3654f6f2
Magus <magus@bgforge.net>
2024-04-22 13:57:51 +0700

    Sync missing messages with UPU, cleanup unused references. See https://github.com/BGforgeNet/Fallout2_Unofficial_Patch/issues/105

--- scripts_src/headers/slvcamp.h
+++ scripts_src/headers/slvcamp.h
@@ -18,7 +18,6 @@
 #define MVAR_Take_Slaves                        (3)
 #define MVAR_Can_Check_Slaves                   (4)
 #define MVAR_Guide_Sister                       (5)
-#define MVAR_Came_With_Rangers                  (7)
 
 #define slaves_in_big_pen(x)     tile_in_tile_rect(15936, 15922, 18536, 18322, x)
 #define slaves_in_small_pen(x)   tile_in_tile_rect(12670, 12658, 13470, 13458, x)
